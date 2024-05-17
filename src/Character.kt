edecf9402d2ef7cbeb3a8a3e21ddadbc4546e782
eric <nukenin.konoha@gmail.com>
2024-05-17 13:07:06 +0700

    feature: add type data Character

---
+++ src/Character.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val charE: Char = 'E'
+    val charR: Char = 'R'
+    val charI: Char = 'I'
+    val charC: Char = 'C'
+
+    println(charE)
+    println(charR)
+    println(charI)
+    println(charC)
+}
