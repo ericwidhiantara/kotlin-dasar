ca462ad1fdbd5456ebac1954f8799341295ae55a
eric <nukenin.konoha@gmail.com>
2024-05-19 22:14:00 +0700

    refactor: modify If Expression

---
+++ src/IfExpression.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val nilai: Int = 75
+
+   if(nilai > 75) {
+       println("Selamat ya!")
+   } else if (nilai == 75) {
+       println("Tidak apa-apa")
+   } else {
+       println("Silahkan ikuti remidial")
+   }
+}
