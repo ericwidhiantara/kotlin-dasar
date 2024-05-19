122f8b543c7841ad5b888cd6715c2909c06d91ce
eric <nukenin.konoha@gmail.com>
2024-05-19 20:03:07 +0700

    feature: add Math Operation

---
+++ src/MathOperation.kt
@@ -0,0 +1,47 @@
+fun main() {
+    var result = 5 + 3
+    println(result)
+    result = 5 - 3
+    println(result)
+    result = 5 * 3
+    println(result)
+    result = 5 / 3
+    println(result)
+    result = 15 % 3
+    println(result)
+
+    // Augmented Assignment
+    var number = 10
+    number += 10
+    println(number)
+    number -= 5
+    println(number)
+    number *= 2
+    println(number)
+    number /= 2
+    println(number)
+    number %= 3
+    println(number)
+
+    // Unary Operator
+    val a = 10
+    val b = -a
+    val c = a.unaryMinus()
+    val d = +a
+    val e = a.unaryPlus()
+    val f = a.inc()
+    val g = a.dec()
+    val h = a.inv()
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+
+
+
+
+
+}
