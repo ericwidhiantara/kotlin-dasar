cbd21e6dc9ea153f298d9402ae984384103b4fb4
eric <nukenin.konoha@gmail.com>
2024-05-19 20:11:34 +0700

    feature: add Compare Operator

---
+++ src/CompareOperator.kt
@@ -0,0 +1,30 @@
+fun main() {
+    val a: Int = 10
+    val b: Int = 20
+
+    println(a == b)
+    println(a != b)
+    println(a > b)
+    println(a < b)
+    println(a >= b)
+    println(a <= b)
+    println("a"<"b")
+    println("a">"b")
+    println("a">="b")
+    println("a"<="b")
+    println("a"=="b")
+    println("a"!="b")
+    println("a"==="b")
+    println("a"!=="b")
+    println("a" in "b")
+    println("a" !in "b")
+    println("a" in "a")
+    println("a" !in "a")
+    println("a" in "ab")
+    println("a" !in "ab")
+    println("a" in "ba")
+    println("a" !in "ba")
+    println("a" in "abc")
+
+
+}
